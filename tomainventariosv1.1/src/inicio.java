a521cff3d5fd043125560c25910b77d05c80f77f
jfarias <jfarias@DESKTOP-3HSJ9F6.mshome.net>
2018-09-13 14:03:38 -0500

    CAMBIO1

--- tomainventariosv1.1/src/inicio.java
+++ tomainventariosv1.1/src/inicio.java
@@ -33,6 +33,9 @@ public class inicio extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
+		
+		
+		HOLA JOVANI
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
