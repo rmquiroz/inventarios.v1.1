7056dd9de012a719f5ebb24b9b81a9fc807c3862
Rodolfo Quiroz <rquiroz@4eglobal.com>
2018-11-29 11:23:27 -0600

    ESCOGE RED EXTERNA O 4E

--- tomainventariosv1.1/src/utilerias/postgresql.java
+++ tomainventariosv1.1/src/utilerias/postgresql.java
@@ -1,4 +1,6 @@
 package utilerias;
+import java.io.IOException;
+import java.net.UnknownHostException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -7,48 +9,68 @@ public class postgresql {
 	public static Connection getConexion() {
 
 		Connection con = null;
-		try {
+			try {
+			//String conOp=Conexion.Openbravo();
 			Class.forName("org.postgresql.Driver");
 			String url = "jdbc:postgresql://10.1.250.24:5932/inventarios";
 			String usuario = "postgres";
 			String pass = "s3st2m1s4e";
-
+			
 			con = DriverManager.getConnection(url, usuario, pass);
 
 		} catch (ClassNotFoundException e) {
-
 			System.out.println("Conexion Fallida DRIVER------>>>");
-
 			e.printStackTrace();
 		} catch (SQLException e) {
-			System.out.println("Conexion BD NO CONECTA------>>>");
-			e.printStackTrace();
-		}
+			try{
+			
+			Class.forName("org.postgresql.Driver");
+			String url = "jdbc:postgresql://201.149.89.164:5932/inventarios";
+			String usuario = "postgres";
+			String pass = "s3st2m1s4e";
+			
+			con = DriverManager.getConnection(url, usuario, pass);
+			} catch (ClassNotFoundException ex) {
+				System.out.println("Conexion Fallida DRIVER------>>>");
+				e.printStackTrace();
+			} catch (SQLException ex) {
+				System.out.println("Conexion BD NO CONECTA------>>>");
+			}
+		} 
 		return con;
-
 	}
+	
 	public static Connection getConexionOpen() {
 
 		Connection con = null;
-		try {
+			try {
+			//String conOp=Conexion.Openbravo();
 			Class.forName("org.postgresql.Driver");
 			String url = "jdbc:postgresql://10.1.250.20:5932/openbravo";
 			String usuario = "postgres";
 			String pass = "s3st2m1s4e";
-
+			
 			con = DriverManager.getConnection(url, usuario, pass);
 
 		} catch (ClassNotFoundException e) {
-
 			System.out.println("Conexion Fallida DRIVER------>>>");
-
 			e.printStackTrace();
 		} catch (SQLException e) {
-			System.out.println("Conexion BD NO CONECTA------>>>");
-			e.printStackTrace();
-		}
+			try{
+			
+			Class.forName("org.postgresql.Driver");
+			String url = "jdbc:postgresql://201.149.89.163:5932/openbravo";
+			String usuario = "postgres";
+			String pass = "s3st2m1s4e";
+			
+			con = DriverManager.getConnection(url, usuario, pass);
+			} catch (ClassNotFoundException ex) {
+				System.out.println("Conexion Fallida DRIVER------>>>");
+				e.printStackTrace();
+			} catch (SQLException ex) {
+				System.out.println("Conexion BD NO CONECTA------>>>");
+			}
+		} 
 		return con;
-
 	}
-	
 }
